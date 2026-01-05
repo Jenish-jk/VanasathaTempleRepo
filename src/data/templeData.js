4694f0633257dffe5a70023bce0608b5950467a9
Jenish J <jenishjk261@gmail.com>
2026-01-05 10:29:26 +0530

    Update location and maps for temple data

--- src/data/templeData.js
+++ src/data/templeData.js
@@ -4,8 +4,8 @@ export const templeData = {
   tagline: "A Sacred Abode of Dharma, Devotion, and Tradition",
 
   // Update these if you want
-  location: "Kerala, India",
-  maps: "https://maps.google.com",
+  location: "Mekkamandapam, TamilNadu, India",
+  maps: "google.com/maps/place/78M2%2B8MR+Arulmigu+Thampuraan+Kovil,+V.M.P,+Mekkamandapam+-+Moolachal+Rd,+Tamil+Nadu+629166/data=!4m2!3m1!1s0x3b04561c676e54e1:0x1dbe8b4595ebae2c?utm_source=mstt_1&entry=gps&coh=192189&g_ep=CAESCjExLjEzNy4xMDMYACDXggMqfiw5NDIwNzgwNSw5NDIyMzI5OSw5NDIxNjQxMyw5NDIxMjQ5Niw5NDIxMjY2Niw5NDIwNzM5NCw5NDIwNzUwNiw5NDIwODUwNiw5NDIxNzUyMyw5NDIxODY1Myw5NDIyOTgzOSw0NzA4NzExOCw0NzA4NDM5Myw5NDIxMzIwMEICSU4%3D&g_st=ac",
   instagram: "https://www.instagram.com/vanasastha_thampuraan/",
   instagramHandle: "vanasastha_thampuraan",
 
