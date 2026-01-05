e4759411f1b7789c1cd7b475324f0b8606f1c2a6
Jenish J <jenishjk261@gmail.com>
2026-01-05 10:33:22 +0530

    Update maps link for temple location

--- src/data/templeData.js
+++ src/data/templeData.js
@@ -5,7 +5,7 @@ export const templeData = {
 
   // Update these if you want
   location: "Mekkamandapam, TamilNadu, India",
-  maps: "google.com/maps/place/78M2%2B8MR+Arulmigu+Thampuraan+Kovil,+V.M.P,+Mekkamandapam+-+Moolachal+Rd,+Tamil+Nadu+629166/data=!4m2!3m1!1s0x3b04561c676e54e1:0x1dbe8b4595ebae2c?utm_source=mstt_1&entry=gps&coh=192189&g_ep=CAESCjExLjEzNy4xMDMYACDXggMqfiw5NDIwNzgwNSw5NDIyMzI5OSw5NDIxNjQxMyw5NDIxMjQ5Niw5NDIxMjY2Niw5NDIwNzM5NCw5NDIwNzUwNiw5NDIwODUwNiw5NDIxNzUyMyw5NDIxODY1Myw5NDIyOTgzOSw0NzA4NzExOCw0NzA4NDM5Myw5NDIxMzIwMEICSU4%3D&g_st=ac",
+  maps: "https://www.google.com/maps/place/Arulmigu+Sree+Vanasastha+Thampuraan+ThiruKovil/@8.283462,77.3016438,974m/data=!3m2!1e3!4b1!4m6!3m5!1s0x3b04561c676e54e1:0x1dbe8b4595ebae2c!8m2!3d8.283462!4d77.3016438!16s%2Fg%2F11c533cfqn",
   instagram: "https://www.instagram.com/vanasastha_thampuraan/",
   instagramHandle: "vanasastha_thampuraan",
 
